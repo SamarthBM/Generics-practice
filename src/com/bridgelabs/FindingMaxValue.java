70cb396232faffd75f927ccc233fa09f0c885141
SamarthBM <samarthbm18@gmail.com>
2021-07-14 19:23:46 +0530

    Initial commit

---
+++ src/com/bridgelabs/FindingMaxValue.java
@@ -0,0 +1,18 @@
+/****************************************************************
+ * Purpose : To use generic method to find maximum value.
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.bridgelabs;
+
+
+public class FindingMaxValue {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
